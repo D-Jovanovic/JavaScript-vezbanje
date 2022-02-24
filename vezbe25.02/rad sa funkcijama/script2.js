052c792780db761306843b6fb1b292cd68ee4c17
D-Jovanovic <d.jovanovic.it@gmail.com>
2022-02-24 15:23:58 +0100

    nasatava vezbanja funkcija

--- vezbe25.02/rad sa funkcijama/script2.js
+++ vezbe25.02/rad sa funkcijama/script2.js
@@ -9,6 +9,35 @@ function main() {
     console.log(neamPojma(1, 1, 1));
     console.log(zbirDvaBroja(2, 3));
     console.log(minDvaBroja(2, 4));
+
+    let a = [1, 2, 3];
+    fun1(a);
+    console.log(a); //rezultat je niz 1 2 3
+
+    let b = [1, 2, 3];
+    fun2(b);
+    console.log(b); //rez 1 2 3 4
+
+    let c = {
+        ime: "Damjan",
+        prezime:  "Jovanovic"
+    };
+
+    fun3(c);
+    console.log(c);
+    //REFERENCA NA FUNKCIJU KAO ARGUMENT FUNKCIJE
+    //negde u memoriji se stvara objekat struktura
+    let struktura = {
+        rezultat: 1,
+        koraci: 0
+    }
+
+            //1. referenca ka funA 2. referenca ka strukturi 3.prosledjuje se vrednsot
+    delegat(funA, struktura, 7); // koraci = 1, rezultat = 8
+    delegat(funB, struktura, 2); // koraci = 2, rezultat = 6
+    delegat(funA, struktura, 4); // koraci = 3, rezultat = 10
+
+    console.log(struktura);
 }
 
 function sumaDvaBroja(prviBroj, drugiBroj) {
@@ -73,4 +102,29 @@ let minDvaBroja = (a, b) => {
     }
 };
 
+//prosledjivanje vrednosti funkcijama
+function fun1(a) {
+    a = "test"; //dodela vrednosti
+}
+
+function fun2(b) {
+    b.push(4);
+}
+
+function fun3(c) {
+    c.ime = "DAMJAN";
+}
+
+function funA(a, b){
+    a.rezultat += b;
+}
+
+function funB(a, b){
+    a.rezultat -= b;
+}
+
+function delegat(nekaFunkcija, a, b){
+    a.koraci += 1;
+    nekaFunkcija(a, b);
+}
 
